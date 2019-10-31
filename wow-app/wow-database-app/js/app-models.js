7ee3b20c9280d3b34795ebb7e05795d5a5c69319
samtrextom <46818719+samtrextom@users.noreply.github.com>
2019-10-30 19:02:43 -0500

    Update app-models.js
    deleting database info

--- wow-app/wow-database-app/js/app-models.js
+++ wow-app/wow-database-app/js/app-models.js
@@ -20,13 +20,13 @@ var Item = function(){
 // TODO: Insert firebase config/init here
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBKdyYt9vsJKwSwFLZkadd3Eo77dxQAe58",
-    authDomain: "wow-item-database.firebaseapp.com",
-    databaseURL: "https://wow-item-database.firebaseio.com",
-    projectId: "wow-item-database",
-    storageBucket: "wow-item-database.appspot.com",
-    messagingSenderId: "509245571819",
-    appId: "1:509245571819:web:de4f388cb27ce9fa1534ae"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
